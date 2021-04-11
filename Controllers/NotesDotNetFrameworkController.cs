6ff9f9cf8e5e810485427aa740aa2874549ad5d4
Willie <longhorngraduate@yahoo.com>
2021-04-10 23:15:57 -0500

    Created new NotesDotNetFramework controller and views

---
+++ Controllers/NotesDotNetFrameworkController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WillieSandoval_2_28_2021.Controllers
+{
+    public class NotesDotNetFrameworkController : Controller
+    {
+        public IActionResult DifferencesBetweenFrameworks()
+        {
+            ViewData["Title"] = "Differences Between Frameworks";
+            return View();
+        }
+
+        public IActionResult CommandLineInterface()
+        {
+            ViewData["Title"] = "Command Line Interface";
+            return View();
+        }
+
+        public IActionResult DotNETMVCsFolderAndFiles()
+        {
+            ViewData["Title"] = "ASP.NET MVC's Folder and Files";
+            return View();
+        }
+
+        public IActionResult UnitTesting()
+        {
+            ViewData["Title"] = "Unit Testing";
+            return View();
+        }
+
+        public IActionResult CSharp()
+        {
+            ViewData["Title"] = "C#";
+            return View();
+        }
+    }
+}
